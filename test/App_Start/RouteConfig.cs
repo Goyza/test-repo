d9f1e469eb712b5de9bb42bd071df12de2136edf
Pavel Bespalov <pavel.bespalov@solidpark.se>
2018-04-18 14:27:45 +0200

    test changes

--- test/App_Start/RouteConfig.cs
+++ test/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace test
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            //test
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
